f5d5c3eedb2162614de8123471ebc88b74ffd88c
Fran Ruiz <francoruizgabriel@gmail.com>
2025-04-11 09:48:35 -0300

    side menu

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,12 @@
 
-    // Inicializar tooltips
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// Inicializar tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    // Scroll a secciones
-    function scrollToSection(id) {
-      document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-    }
+// Scroll a secciones
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.querySelector(".toggle-button");
